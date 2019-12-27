122f31a334c90ccfadab67043f4585aa70733544
yuta24 <yuta24@bivre.com>
2019-12-27 09:36:23 +0900

    Add ToDo

---
+++ ToDo/ToDo.h
@@ -0,0 +1,19 @@
+//
+//  ToDo.h
+//  ToDo
+//
+//  Created by CTO-Catalina-test on 2019/12/27.
+//  Copyright © 2019 Yu Tawata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToDo.
+FOUNDATION_EXPORT double ToDoVersionNumber;
+
+//! Project version string for ToDo.
+FOUNDATION_EXPORT const unsigned char ToDoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToDo/PublicHeader.h>
+
+
